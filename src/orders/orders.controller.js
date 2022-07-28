89516d0f681462bc93b57f8d6f50ce02fa0d9f49
natalie palacios <natalie.pm82@outlook.com>
2022-07-28 14:02:08 -0700

    Update orders.controller.js with comments

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -12,8 +12,11 @@ const nextId = require("../utils/nextId");
 //retrieve a list of all orders
 function list(req, res) {
   res.json({ data: orders });
-} 
+};
 
+/*middleware functions-additional validations*/
+
+//middleware function to validate if "deliverTo" property is in request
 function deliverToPropertyMissing(req, res, next) {
   const { data: { deliverTo } = {} } = req.body;
   if (deliverTo) {
@@ -25,8 +28,8 @@ function deliverToPropertyMissing(req, res, next) {
     status: 400,
     message: "Order must include a deliverTo",
   });
-} 
-
+};
+//middleware function to validate if "deliverTo" property is empty
 function deliverToPropertyEmpty(req, res, next) {
   const { data: { deliverTo } = {} } = req.body;
   if (deliverTo === "") {
@@ -36,8 +39,8 @@ function deliverToPropertyEmpty(req, res, next) {
     });
   }
   return next();
-} 
-
+}; 
+//middleware function to validate if "mobileNumber" property is in request
 function mobileNumberPropertyMissing(req, res, next) {
   const { data: { mobileNumber } = {} } = req.body;
   if (mobileNumber) {
@@ -48,8 +51,8 @@ function mobileNumberPropertyMissing(req, res, next) {
     status: 400,
     message: "Order must include a mobileNumber",
   });
-} 
-
+};
+//middleware function to validate if "mobileNumber" property is empty
 function mobileNumberPropertyEmpty(req, res, next) {
   const { data: { mobileNumber } = {} } = req.body;
   if (mobileNumber === "") {
@@ -59,8 +62,8 @@ function mobileNumberPropertyEmpty(req, res, next) {
     });
   }
   return next();
-} 
-
+};
+//middleware function to validate if "dishes" property is in request
 function dishesPropertyMissing(req, res, next) {
   const { data: { dishes } = {} } = req.body;
   if (dishes) {
@@ -71,8 +74,8 @@ function dishesPropertyMissing(req, res, next) {
     status: 400,
     message: "Order must include a dish",
   });
-} 
-
+};
+//middleware function to validate if "dishes" property is an array
 function dishesPropertyArray(req, res, next) {
   const { data: { dishes } = {} } = req.body;
   if (Array.isArray(dishes)) {
@@ -82,8 +85,8 @@ function dishesPropertyArray(req, res, next) {
     status: 400,
     message: "Order must include at least one dish",
   });
-}
-
+};
+//middleware function to validate if "dishes" property is empty
 function dishesPropertyEmpty(req, res, next) {
   const { data: { dishes } = {} } = req.body;
   if (dishes.length > 0) {
@@ -93,8 +96,8 @@ function dishesPropertyEmpty(req, res, next) {
     status: 400,
     message: "Order must include at least one dish",
   });
-}
-
+};
+//middleware function to validate if dish "quantity" property is in request
 function dishQuantityPropertyMissing(req, res, next) {
   const { data: { dishes } = {} } = req.body;
   const quantityMissing = dishes.find((dish) => !dish.quantity);
@@ -106,8 +109,8 @@ function dishQuantityPropertyMissing(req, res, next) {
     });
   }
   return next();
-}
-
+};
+//middleware function to validate if dish "quantity" property is 0 or less
 function dishQuantityisZero(req, res, next) {
   const { data: { dishes } = {} } = req.body;
   const quantityLessThanOne = dishes.find((dish) => dish.quantity < 1);
@@ -120,8 +123,8 @@ function dishQuantityisZero(req, res, next) {
   }
 
   return next();
-} 
-
+}; 
+//middleware function to validate if dish "quantity" property is an integer
 function dishQuantityPropertyNotInteger(req, res, next) {
   const { data: { dishes } = {} } = req.body;
   const quantityNotInteger = dishes.find(
@@ -135,8 +138,8 @@ function dishQuantityPropertyNotInteger(req, res, next) {
     });
   }
   return next();
-} 
-
+};
+//create a new order
 function create(req, res) {
   const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
   const newId = new nextId();
@@ -150,8 +153,10 @@ function create(req, res) {
 
   orders.push(newOrder);
   res.status(201).json({ data: newOrder });
-} 
+};
+/*middleware functions for orders by ID */
 
+//check if order exists
 function orderExists(req, res, next) {
   const { orderId } = req.params;
   const foundOrder = orders.find((order) => order.id === orderId);
@@ -164,12 +169,12 @@ function orderExists(req, res, next) {
     status: 404,
     message: `Order id not found: ${orderId}`,
   });
-} 
+};
 
 function read(req, res) {
   res.json({ data: res.locals.order });
-} 
-
+};
+//check if order id matches with request id
 function orderIdMatch(req, res, next) {
   const { data: { id } = {} } = req.body;
   const { orderId } = req.params;
@@ -180,9 +185,9 @@ function orderIdMatch(req, res, next) {
     status: 400,
     message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
   });
-} 
+}; 
 
-// This a Middleware function to check if status property exists
+//middleware function to check if status property exists
 function statusPropertyExists(req, res, next) {
   const { data: { status } = {} } = req.body;
   if (status) {
@@ -193,9 +198,9 @@ function statusPropertyExists(req, res, next) {
     status: 400,
     message: `Order must have a status of pending, preparing, out-for-delivery, delivered.`,
   });
-} 
+};
 
-//middleware function to check if status property is correct value
+//middleware function to check if order status property is correct value
 function statusPropertyCorrect(req, res, next) {
   const { data: { status } = {} } = req.body;
   if (
@@ -210,9 +215,9 @@ function statusPropertyCorrect(req, res, next) {
     status: 400,
     message: `Order must have a status of pending, preparing, out-for-delivery, delivered.`,
   });
-} 
+};
 
-// This a Middleware function to check if order has been delivered
+//middleware function to check if order has been delivered
 function statusPropertyDelivered(req, res, next) {
   const { data: { status } = {} } = req.body;
   if (status === "delivered") {
@@ -222,8 +227,8 @@ function statusPropertyDelivered(req, res, next) {
     });
   }
   return next();
-} 
-
+};
+//update order
 function update(req, res) {
   const order = res.locals.order;
   const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
@@ -233,8 +238,8 @@ function update(req, res) {
   order.dishes = dishes;
 
   res.json({ data: order });
-} 
-
+};
+//middleware function to check if order status is pending
 function statusPropertyPending(req, res, next) {
   const order = res.locals.order;
   const { status } = order;
@@ -245,15 +250,15 @@ function statusPropertyPending(req, res, next) {
     status: 400,
     message: `An order cannot be deleted unless it is pending.`,
   });
-} 
-
+};
+//delete order
 function destroy(req, res) {
   const { orderId } = req.params;
   const index = orders.findIndex((order) => order.id === orderId);
   orders.splice(index, 1);
 
   res.sendStatus(204);
-}
+};
 
 module.exports = {
   list,
